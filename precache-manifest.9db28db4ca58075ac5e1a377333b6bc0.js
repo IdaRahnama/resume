0ab8cd685d22a9fa583a7de31412d6c1c2402ac4
IdaRahnama <idadoost@gmail.com>
2019-10-20 13:01:33 +0200

    Updates

---
+++ precache-manifest.9db28db4ca58075ac5e1a377333b6bc0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd950860a78ddd180439735781fe96d0",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "9cc98cfc7e2019c37692",
+    "url": "/resume/static/css/main.d8a4a522.chunk.css"
+  },
+  {
+    "revision": "198c2e371bffff030104",
+    "url": "/resume/static/js/2.1c9e7a8f.chunk.js"
+  },
+  {
+    "revision": "9cc98cfc7e2019c37692",
+    "url": "/resume/static/js/main.eb72155c.chunk.js"
+  },
+  {
+    "revision": "b3bda273d2593438a840",
+    "url": "/resume/static/js/runtime-main.5fa69b1f.js"
+  },
+  {
+    "revision": "d22ee3727a7216019c3848df6eafa024",
+    "url": "/resume/static/media/GitHub.d22ee372.png"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/resume/static/media/css-3.b848d5d9.png"
+  },
+  {
+    "revision": "511db6a19b87d3634f198a6c7ffb2cb7",
+    "url": "/resume/static/media/hashtag.511db6a1.png"
+  },
+  {
+    "revision": "675307aced93300a2f98bdf472c98d25",
+    "url": "/resume/static/media/html5.675307ac.png"
+  },
+  {
+    "revision": "74e943d82452f81f6a0bb0b51ac6785d",
+    "url": "/resume/static/media/java.74e943d8.png"
+  },
+  {
+    "revision": "20e4fd7eed6831c1778aa056a32b3244",
+    "url": "/resume/static/media/reem-kufi-v4-latin-regular.20e4fd7e.woff"
+  },
+  {
+    "revision": "d8f8811d59f2a4935f145012efb7e98f",
+    "url": "/resume/static/media/reem-kufi-v4-latin-regular.d8f8811d.woff2"
+  },
+  {
+    "revision": "9a8a1f8c8f9860224f449c21a3bd76d4",
+    "url": "/resume/static/media/roboto-100.9a8a1f8c.woff2"
+  },
+  {
+    "revision": "c0cc9c92fb877993ea6d421c31d33e09",
+    "url": "/resume/static/media/roboto-300.c0cc9c92.woff2"
+  },
+  {
+    "revision": "ece6673e477b4d7aca12f04dace5ed60",
+    "url": "/resume/static/media/roboto-400.ece6673e.woff2"
+  },
+  {
+    "revision": "2e17a64836c0412759543f90460d0ed1",
+    "url": "/resume/static/media/roboto-900.2e17a648.woff2"
+  }
+]);
